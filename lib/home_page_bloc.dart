48aea5baf10b74c68967aec3e5e26bfbe4faa368
DaffaPutra7 <putradika3604@gmail.com>
2025-05-16 02:04:19 +0700

    membuat file home_page_bloc.dart yang diisi stateles widget

---
+++ lib/home_page_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePageBloc extends StatelessWidget {
+  const HomePageBloc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
