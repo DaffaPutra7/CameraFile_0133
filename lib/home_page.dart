8827d95c30c97d63ef4550d6af4fa21799bbf3c0
DaffaPutra7 <putradika3604@gmail.com>
2025-05-15 21:22:33 +0700

    buat fungsi takePicture

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:camera_pamlanjut/camera_page.dart';
+import 'package:camera_pamlanjut/storage_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -20,7 +22,19 @@ class _FullPageState extends State<FullPage> {
     await Permission.manageExternalStorage.request();
   }
 
-  
+  Future<void> _takePicture() async {
+    await _requestPermissions();
+    final File? result = await Navigator.push<File?>(
+      context,
+      MaterialPageRoute(builder: (_) => const CameraPage()),
+    );
+    if (result != null) {
+      final saved = await StorageHelper.saveImage(result, 'camera');
+      setState(() => _imageFile = saved);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Disimpan: ${saved.path}')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
