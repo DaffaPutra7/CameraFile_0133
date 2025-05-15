f94efaf50d71468a4ca00b5713bba8e59c9bc7f2
DaffaPutra7 <putradika3604@gmail.com>
2025-05-16 00:50:53 +0700

    memperbaiki code dan membuat fungsi untuk bisa melakukan tap pada layar kamera dan bisa memilih/mengambil gambar dari galeri

--- lib/bloc/camerafile_bloc.dart
+++ lib/bloc/camerafile_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
 
 part 'camerafile_event.dart';
@@ -30,14 +31,14 @@ class CamerafileBloc extends Bloc<CamerafileEvent, CamerafileState> {
     Emitter<CamerafileState> emit,
   ) async {
     _cameras = await availableCameras();
-    await _setupController(emit);
+    await _setupController(0, emit);
   }
 
   Future<void> _onSwitch(SwitchCamera event, Emitter<CamerafileState> emit) async {
     if (state is! CameraReady) return;
     final s = state as CameraReady;
     final next = (s.selectedIndex + 1) % _cameras.length;
-    await _setupController(emit, next);
+    await _setupController(next, emit, previous: s);
   }
 
   Future<void> _onToggleFlash(
@@ -46,11 +47,11 @@ class CamerafileBloc extends Bloc<CamerafileEvent, CamerafileState> {
   ) async {
     if (state is! CameraReady) return;
     final s = state as CameraReady;
-    final next = s.flashMode == FlashMode.auto
-        ? FlashMode.always
-        : s.flashMode == FlashMode.always
-            ? FlashMode.off
-            : FlashMode.auto;
+    final next = s.flashMode == FlashMode.off
+        ? FlashMode.auto
+        : s.flashMode == FlashMode.auto
+            ? FlashMode.always
+            : FlashMode.off;
     await s.controller.setFlashMode(next);
     emit(s.copyWith(flashMode: next));
   }
@@ -62,6 +63,32 @@ class CamerafileBloc extends Bloc<CamerafileEvent, CamerafileState> {
     if (state is! CameraReady) return;
     final s = state as CameraReady;
     final file = await s.controller.takePicture();
-    emit(s.copyWith(imageFile: File(file.path)));
+    event.onPictureTaken(File(file.path));
   }
+
+  Future<void> _onTapToFocus(TapToFocus event, Emitter<CamerafileState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final relative = Offset(
+      event.position.dx / event.previewSize.width,
+      event.position.dy / event.previewSize.height,
+    );
+    await s.controller.setFocusPoint(relative);
+    await s.controller.setExposurePoint(relative);
+  }
+
+  Future<void> _onPickGallery(
+    PickImageFromGallery event,
+    Emitter<CamerafileState> emit,
+  ) async {
+    if (state is! CameraReady) return;
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+    final file = File(picked!.path);
+      emit((state as CameraReady).copyWith(
+        imageFile: file,
+        snackbarMessage: 'Berhasil memilih dari galeri.',
+      ));
+  }
+
 }
